10fb50f63b6fe407b6c8c9387990f3a3cf7f78df
ttkeele <3264@holbertonschool.com>
2021-12-21 14:31:29 +0000

    added imports,host and port

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,15 +2,16 @@
 """
 the app module
 """
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
+from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_storage(exception):
@@ -26,5 +27,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST"), port=getenv("HBNB_API_PORT"),
-            threaded=True)
+    host = getenv("HBNB_API_HOST", '0.0.0.0')
+    port = getenv("HBNB_API_PORT", '5000')
+    app.run(host=host, port=port, threaded=True)
