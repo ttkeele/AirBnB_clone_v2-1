c3119f38362e6112549e03955c31ff833491c059
ttkeele <3264@holbertonschool.com>
2021-12-21 16:48:12 +0000

    tweaking

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
@@ -27,6 +28,5 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", '0.0.0.0')
-    port = getenv("HBNB_API_PORT", '5000')
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST"), port=getenv("HBNB_API_PORT"),
+            threaded=True)
