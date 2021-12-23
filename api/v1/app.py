96c7e8bfe1fd78ab176f977091441d914091052e
ttkeele <3264@holbertonschool.com>
2021-12-23 16:45:00 +0000

    fixed spacing

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,6 @@ from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
